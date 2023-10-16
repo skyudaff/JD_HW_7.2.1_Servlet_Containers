a9aebd4bdba87fbe6d13c7ce1bdbe96605d9e784
Vladimir Razuvaev <skyudaff@mail.ru>
2023-10-16 17:22:38 +0300

    Initial commit

---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,55 @@
+package org.example.controller;
+
+import com.google.gson.Gson;
+import org.example.model.Post;
+import org.example.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        var data = service.all();
+        var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        var data = service.getById(id);
+        var gson = new Gson();
+        if (data != null) {
+            writeJsonResponse(response.getWriter(), gson, data);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        var gson = new Gson();
+        var post = gson.fromJson(body, Post.class);
+        var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) {
+        service.removeById(id);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void writeJsonResponse(Writer writer, Gson gson, Object data) throws IOException {
+        writer.write(gson.toJson(data));
+    }
+}
---
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -0,0 +1,64 @@
+package org.example.servlet;
+
+
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POSTS_REGEX = "/api/posts/\\d+";
+
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            var path = req.getRequestURI();
+            var method = req.getMethod();
+
+            if (method.equals(GET) && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(API_POSTS_REGEX)) {
+                final var id = parseLong(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(API_POSTS_REGEX)) {
+                final var id = parseLong(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private Long parseLong(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
