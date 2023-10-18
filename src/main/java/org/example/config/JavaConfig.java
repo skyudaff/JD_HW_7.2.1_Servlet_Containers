aa906ee761a485406e603fe943f7ecd4b3ab975f
Vladimir Razuvaev <skyudaff@mail.ru>
2023-10-18 21:50:10 +0300

    added JavaConfig.class

---
+++ src/main/java/org/example/config/JavaConfig.java
@@ -0,0 +1,26 @@
+package org.example.config;
+
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public PostController postController() {
+        return new PostController(postService());
+    }
+
+    @Bean
+    public PostService postService() {
+        return new PostService(postRepository());
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+}
